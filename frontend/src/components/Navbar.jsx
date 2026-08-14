5730fb13a3a3f2d4f157d1436937fd64b3d5c43d
Shivani Vishwakarma <shivaniv5354@gmail.com>
2026-08-14 23:31:34 +0530

    Make blog platform responsive across devices

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,41 +15,63 @@ function Navbar() {
           BlogSpace
         </Link>
 
-        <div className="d-flex align-items-center gap-3">
-          <Link to="/" className="nav-link text-white">
-            Home
-          </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNavbar"
+          aria-controls="mainNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          {user ? (
-            <>
-              <Link to="/profile" className="nav-link text-white">
-                Profile
-              </Link>
+        <div className="collapse navbar-collapse" id="mainNavbar">
+          <div className="navbar-nav ms-auto align-items-lg-center gap-lg-3">
+            <Link to="/" className="nav-link text-white">
+              Home
+            </Link>
 
-              <Link to="/create-post" className="btn btn-light btn-sm px-3">
-                Write
-              </Link>
+            {user ? (
+              <>
+                <Link to="/profile" className="nav-link text-white">
+                  Profile
+                </Link>
 
-              <span className="text-white">Hi, {user.name}</span>
+                <Link
+                  to="/create-post"
+                  className="btn btn-light btn-sm px-3 my-2 my-lg-0"
+                >
+                  Write
+                </Link>
 
-              <button
-                className="btn btn-outline-light btn-sm"
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link to="/login" className="nav-link text-white">
-                Login
-              </Link>
+                <span className="text-white user-greeting">
+                  Hi, {user.name}
+                </span>
 
-              <Link to="/register" className="btn btn-light btn-sm px-3">
-                Register
-              </Link>
-            </>
-          )}
+                <button
+                  className="btn btn-outline-light btn-sm"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to="/login" className="nav-link text-white">
+                  Login
+                </Link>
+
+                <Link
+                  to="/register"
+                  className="btn btn-light btn-sm px-3 my-2 my-lg-0"
+                >
+                  Register
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </nav>
